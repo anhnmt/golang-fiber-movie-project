80a45434968f028997f9eb96b0c8a9d391257cdc
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-06-21 21:29:42 +0700

    Update model, ...

--- app/model/Country.go
+++ app/model/Country.go
@@ -1,7 +1,7 @@
 package model
 
 type Country struct {
-	CountryId uint   `gorm:"primarykey" json:"country_id"`
+	CountryId uint   `gorm:"primaryKey" json:"country_id"`
 	Name      string `gorm:"not null;unique" json:"name"`
 	Slug      string `gorm:"not null;unique" json:"slug"`
 	Status    int8   `gorm:"default:1" json:"status"`
